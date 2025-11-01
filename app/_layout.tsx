3aa59fcde0220ee43311e49e46be3e90f486a81b
SAIKAT SAMANTA <saikat.samanta@hy-vee.com>
2025-11-01 19:44:01 +0530

    fixes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,6 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "../contexts/AuthContext";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import 'react-circular-progressbar/dist/styles.css';
 
 export default function RootLayout() {
 
